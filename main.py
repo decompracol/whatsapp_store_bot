36bd97f987744553e971a0920628ae530c812560
Jorge Rico <decompracolombia@gmail.com>
2025-06-21 18:04:23 -0500

    Actualizar main.py para usar host 0.0.0.0 y puerto dinámico

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 from flask import Flask
+import os
 from app.whatsapp_webhook import whatsapp_blueprint
 
 app = Flask(__name__)
 app.register_blueprint(whatsapp_blueprint)
 
 if __name__ == '__main__':
-    app.run(port=8000, debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
